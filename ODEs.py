cc19c045b520bd3eca1314953a8d396692125362
avivko <aviv.korman@gmail.com>
2018-11-23 13:29:17 +0100

    solver and plotting implemented for the first part of the paper. Fp is in this version as a constant -> should be made into a function

--- ODEs.py
+++ ODEs.py
@@ -29,7 +29,7 @@ Kifr = 0.5
 Ka = 0.7
 Ki = 0.7
 Kd = 3
-#Fp = 4  Fgf4 concentration ? 
+Fp = 0.6  # Fgf4 should be a function ! this is just for testin
 
 r = 3
 s = 4
@@ -38,29 +38,43 @@ u = 3
 v = 4
 w = 4
 z = 4
+print('hey')
 
 # simulation time
-#start = 0  # min
-#end = 100  # min
-#plotPoints = 1000
-#timeGrid = np.linspace(start, end, plotPoints)
+start = 0  # min
+end = 100  # min
+plotPoints = 100
+# timeGrid = np.linspace(start, end, plotPoints) #right now  not used
 
 
 # returns the ODE as the operator d/dt()
-def dif(x, t):
-    #G, N, FR, ERK = x
-
-    dG = (vsg1 * ((ERK**r) / ((Kag1**r) + (ERK**r))) + vsg2 * ((G**s) / ((Kag2**s) + (G**s)))) * ((Kig**q) / ((Kig**q) + (N**q))) - kdg * G
-    dN = (vsn1 * ((Kin1**u) / ((Kin1**u) + (ERK**u))) + vsn2 * ((N**v) / ((Kan**v) + (N**v)))) * ((Kin2**w) / ((Kin2**w) + (G**w))) - kdn * N
+def dif(t, x):
+    G, N, FR, ERK = x
+    dG = (vsg1 * ((ERK ** r) / ((Kag1 ** r) + (ERK ** r))) + vsg2 * ((G ** s) / ((Kag2 ** s) + (G ** s)))) * (
+            (Kig ** q) / ((Kig ** q) + (N ** q))) - kdg * G
+    dN = (vsn1 * ((Kin1 ** u) / ((Kin1 ** u) + (ERK ** u))) + vsn2 * ((N ** v) / ((Kan ** v) + (N ** v)))) * (
+            (Kin2 ** w) / ((Kin2 ** w) + (G ** w))) - kdn * N
     dFR = vsfr1 * ((Kifr) / (Kifr + N)) + vsfr2 * ((G) / (Kafr + G)) - kdfr * FR
-    dERK = va * FR * ((Fp) / (Kd + Fp)) * ((1 - ERK) / (Ka + 1 - ERK)) - vin * ((ERK) / (Ki + ERK)) # is "vin" the typo friedemann was talking about? becaus i can't find it in the parameters.
-
-    #return [dG, dN, dFR, dERK]
-
+    dERK = va * FR * ((Fp) / (Kd + Fp)) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * ((ERK) / (Ki + ERK))  # vin->vi ; typo?
+    return [dG, dN, dFR, dERK]
 
 
+# solve with V0 as list (different starting points). Max N and G are hardcoded as 2.2
+def varying_v0_solver(step_size):
+    results_list = []
+    step = step_size
+    while step <= 2.2:
+        results_list.append(solve_ivp(dif, (start, end), [step, 0, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0]
+        results_list.append(solve_ivp(dif, (start, end), [0, step, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0]
+        step += step_size
+    return results_list
 
 
-#v0 = [G0,N0,FR0,ERK0]
-#result = solve_ivp(f, y0, timeGrid)
-
+results = varying_v0_solver(0.2)
+print('here?')
+for result in results:
+    x_data = result.y[0]
+    y_data = result.y[1]
+    plt.plot(x_data, y_data, label='G')
+plt.legend(fontsize='small', loc=1)
+plt.show()
