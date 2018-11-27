96b878a1f7cd00105d55a61f7e7d4f4021e9b031
darimchermann <dariusrauch@gmail.com>
2018-11-27 16:08:46 +0100

    First steps implementing the two cell system

--- ODEs.py
+++ ODEs.py
@@ -29,7 +29,7 @@ Kifr = 0.5
 Ka = 0.7
 Ki = 0.7
 Kd = 2
-FpTristablility = 0.06
+FpTristability = 0.06
 Fp0 = 0.03  # as seen in fig. 2
 FPend = 0.11  # as seen in fig. 2
 FR0 = 2.8
@@ -51,6 +51,69 @@ end = 50  # max
 plotPoints = 500
 timeGrid = np.linspace(start, end, plotPoints)
 
+#####two cell model:######
+gam = 0.03  # gamma deviation around the avergage extracellular concentration of Fgf4
+
+def dif_F_synthesis(t,x):
+    dFs1  = vsf * ((N1 ** z)/((Kaf ** z)+(N1 ** z))) - kdf * Fs1 + vex # amount of synthesized Fgf4 bey cell 1
+    dFs2  = vsf * ((N2 ** z)/((Kaf ** z)+(N2 ** z))) - kdf * Fs2 + vex # # amount of synthesized Fgf4 bey cell 2
+    dF = ((dFs1 + sFs2)/(2)) # change of extracellular Fgf4
+    Fp1 = (1-gam) * F #local variabilty of Fgf4
+    Fp2 = (1+gam) * F #local variabilty of Fgf4
+
+# Inital condintions
+G10 = 0
+N10 = 0
+FR10 = 2.8
+ERK10 = 0.25
+F = 0.066
+
+
+
+def dif_Fp1_variable(t, x):
+    G1 = x[0]
+    N1 = x[1]
+    FR1 = x[2]
+    ERK1 = x[3]
+    dG1 = (vsg1 * ((ERK1 ** r) / ((Kag1 ** r) + (ERK1 ** r))) + vsg2 * ((G1 ** s) / ((Kag2 ** s) + (G1 ** s)))) * (
+            (Kig ** q) / ((Kig ** q) + (N1 ** q))) - kdg * G1
+    dN1 = (vsn1 * ((Kin1 ** u) / ((Kin1 ** u) + (ERK1 ** u))) + vsn2 * ((N1 ** v) / ((Kan ** v) + (N1 ** v)))) * (
+            (Kin2 ** w) / ((Kin2 ** w) + (G1 ** w))) - kdn * N1
+    dFR1 = vsfr1 * ((Kifr) / (Kifr + N1)) + vsfr2 * ((G) / (Kafr + G)) - kdfr * FR
+    dERK1 = va * FR * Fp1 / (Kd + Fp1) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * (
+                (ERK) / (Ki + ERK))  # vin->vi ; typo?  # is the maximal time
+    return [dG1, dN1, dFR1, dERK1]
+
+def dif_Fp2_variable(t, x):
+    G2 = x[0]
+    N2 = x[1]
+    FR2 = x[2]
+    ERK2 = x[3]
+    dG2 = (vsg1 * ((ERK2 ** r) / ((Kag1 ** r) + (ERK2 ** r))) + vsg2 * ((G2 ** s) / ((Kag2 ** s) + (G2 ** s)))) * (
+            (Kig ** q) / ((Kig ** q) + (N2 ** q))) - kdg * G2
+    dN2 = (vsn1 * ((Kin1 ** u) / ((Kin1 ** u) + (ERK2 ** u))) + vsn2 * ((N2 ** v) / ((Kan ** v) + (N2 ** v)))) * (
+            (Kin2 ** w) / ((Kin2 ** w) + (G2 ** w))) - kdn * N2
+    dFR2 = vsfr1 * ((Kifr) / (Kifr + N2)) + vsfr2 * ((G2) / (Kafr + G2)) - kdfr * FR2
+    dERK2 = va * FR2 * Fp2 / (Kd + Fp2) * ((1 - ERK2) / (Ka + 1 - ERK2)) - vi * (
+                (ERK2) / (Ki + ERK2))  # vin->vi ; typo?  # is the maximal time
+    return [dG2, dN2, dFR2, dERK2]
+
+
+#trying to solve here:
+
+
+
+
+#tryign to plot here:
+
+
+
+
+
+
+
+
+
 
 # returns the ODE as the operator d/dt(), where Fp is a constant (for phase space diagrams)
 def dif_Fp_const(t, x):
@@ -63,7 +126,7 @@ def dif_Fp_const(t, x):
     dN = (vsn1 * ((Kin1 ** u) / ((Kin1 ** u) + (ERK ** u))) + vsn2 * ((N ** v) / ((Kan ** v) + (N ** v)))) * (
             (Kin2 ** w) / ((Kin2 ** w) + (G ** w))) - kdn * N
     dFR = vsfr1 * ((Kifr) / (Kifr + N)) + vsfr2 * ((G) / (Kafr + G)) - kdfr * FR
-    dERK = va * FR * ((FpTristablility) / (Kd + FpTristablility)) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * (
+    dERK = va * FR * ((FpTristability) / (Kd + FpTristability)) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * (
                 (ERK) / (Ki + ERK))  # vin->vi ; typo?
     return [dG, dN, dFR, dERK]
 
@@ -147,3 +210,4 @@ plt.plot(t_data_bif, Fp_data_bif, label='lower right')
 plot4.set_xlabel('Time', fontsize=15)
 plot4.set_ylabel('Fgf4', fontsize=15)
 plt.show()
+
