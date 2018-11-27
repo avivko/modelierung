db00c7865d9d97065ab073cd938923908cd477ad
avivko <aviv.korman@gmail.com>
2018-11-27 10:20:25 +0100

    two different solvers and functions implemented for phase space (Fp const) and bifurcation diagrams (Fp param)

--- ODEs.py
+++ ODEs.py
@@ -29,6 +29,7 @@ Kifr = 0.5
 Ka = 0.7
 Ki = 0.7
 Kd = 3
+FpTristablility = 0.06
 Fp0 = 0.03  # as seen in fig. 2
 FPend = 0.11  # as seen in fig. 2
 
@@ -44,13 +45,28 @@ z = 4
 
 # simulation time
 start = 0  # min
-end = 100  # max
-plotPoints = 100
+end = 10  # max
+plotPoints = 10
 # timeGrid = np.linspace(start, end, plotPoints) #right now  not used
 
 
-# returns the ODE as the operator d/dt()
-def dif(t, x):
+# returns the ODE as the operator d/dt(), where Fp is a constant (for phase space diagrams)
+def dif_Fp_const(t, x):
+    G = x[0]
+    N = x[1]
+    FR = x[2]
+    ERK = x[3]
+    dG = (vsg1 * ((ERK ** r) / ((Kag1 ** r) + (ERK ** r))) + vsg2 * ((G ** s) / ((Kag2 ** s) + (G ** s)))) * (
+            (Kig ** q) / ((Kig ** q) + (N ** q))) - kdg * G
+    dN = (vsn1 * ((Kin1 ** u) / ((Kin1 ** u) + (ERK ** u))) + vsn2 * ((N ** v) / ((Kan ** v) + (N ** v)))) * (
+            (Kin2 ** w) / ((Kin2 ** w) + (G ** w))) - kdn * N
+    dFR = vsfr1 * ((Kifr) / (Kifr + N)) + vsfr2 * ((G) / (Kafr + G)) - kdfr * FR
+    dERK = va * FR * ((FpTristablility) / (Kd + FpTristablility)) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * ((ERK) / (Ki + ERK))  # vin->vi ; typo?
+    return [dG, dN, dFR, dERK]
+
+
+# returns the ODE as the operator d/dt(), where Fp is a parameter (for bifurcation diagrams)
+def dif_Fp_param(t, x):
     G = x[0]
     N = x[1]
     FR = x[2]
@@ -62,49 +78,52 @@ def dif(t, x):
             (Kin2 ** w) / ((Kin2 ** w) + (G ** w))) - kdn * N
     dFR = vsfr1 * ((Kifr) / (Kifr + N)) + vsfr2 * ((G) / (Kafr + G)) - kdfr * FR
     dERK = va * FR * ((Fp) / (Kd + Fp)) * ((1 - ERK) / (Ka + 1 - ERK)) - vi * ((ERK) / (Ki + ERK))  # vin->vi ; typo?
-    dFp = Fp0+((FPend-Fp0)/(end-start))*t
-    return [dG, dN, dFR, dERK, dFp, t]
-
+    dFp = ((FPend-Fp0)/(50-start))*t  # is the maximal time
+    return [dG, dN, dFR, dERK, dFp]
 
 # solve with V0 as list (different starting points). Max N and G are hardcoded as 2.2
 def varying_v0_solver(step_size):
     results_list = []
     step = step_size
     while step <= 2.2:
-        results_list.append(solve_ivp(dif, (start, end), [step, 0, 0, 0, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0,Fp,t]
-        results_list.append(solve_ivp(dif, (start, end), [0, step, 0, 0, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0,Fp,t]
+        results_list.append(solve_ivp(dif_Fp_const, (start, end), [step, 0.2, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0]
+        results_list.append(solve_ivp(dif_Fp_const, (start, end), [0.2, step, 0, 0]))  # order for v0: [G0,N0,FR0,ERK0]
         step += step_size
     return results_list
 
 
-results = varying_v0_solver(0.2)
+resultsPhaseSpace = varying_v0_solver(0.2)
+resultBifurcation = solve_ivp(dif_Fp_param, (start, end), [0, 0, 0, 0, 0.03])  # order for v0: [G0,N0,FR0,ERK0,Fp])
+
+G_data_bif = resultBifurcation.y[0]
+N_data_bif = resultBifurcation.y[1]
+Fp_data_bif = resultBifurcation.y[4]
+t_data_bif = resultBifurcation.t
 
 # square grid of 4 subplots
 plt.figure(figsize=(9,6)) # generate slightly larger figure
 
-for result in results:
-    G_data = result.y[0]
-    N_data = result.y[1]
-    FR_data = result.y[2]
-    ERK_data = result.y[3]
-    Fp_data = result.y[4]
-    t_data = result.y[5]
-    # access first 3 one with ax.
-    plot1 = plt.subplot(2,2,1)
-    plot1.plot(Fp_data, G_data, label='upper left')
-    plot1.set_xlabel('Fgf4', fontsize=15)
-    plot1.set_ylabel('Gata6', fontsize=15)
-    plot2 = plt.subplot(2,2,2)
-    plot2.plot(Fp_data, N_data, label='upper right')
-    plot2.set_xlabel('Fgf4', fontsize=15)
-    plot2.set_ylabel('Nanog', fontsize=15)
-    plot3 = plt.subplot(2,2,3)
-    plot3.plot(G_data, N_data, label='lower left')
+# Gata6(Fgf4) plot - bifurcation diagram
+plot1 = plt.subplot(2,2,1)
+plot1.plot(Fp_data_bif, G_data_bif, label='upper left')
+plot1.set_xlabel('Fgf4', fontsize=15)
+plot1.set_ylabel('Gata6', fontsize=15)
+# Nanog(Fgf4) plot - bifurcation diagram
+plot2 = plt.subplot(2,2,2)
+plot2.plot(Fp_data_bif, N_data_bif, label='upper right')
+plot2.set_xlabel('Fgf4', fontsize=15)
+plot2.set_ylabel('Nanog', fontsize=15)
+# Nanog(Gata6) plot - phase space diagram
+plot3 = plt.subplot(2,2,3)
+for result in resultsPhaseSpace:
+    G_data_ps = result.y[0]
+    N_data_ps = result.y[1]
+    plot3.plot(G_data_ps, N_data_ps, label='lower left')
     plot3.set_xlabel('Gata6', fontsize=15)
     plot3.set_ylabel('Nanog', fontsize=15)
-    # access the last with plt. (linear)
-    plot4 = plt.subplot(2,2,4)
-    plt.plot(t_data, Fp_data, label='lower right')
-    plot4.set_xlabel('Time', fontsize=15)
-    plot4.set_ylabel('Fgf4', fontsize=15)
+# Fgf4(t) plot from the bifurcation plots
+plot4 = plt.subplot(2,2,4)
+plt.plot(t_data_bif, Fp_data_bif, label='lower right')
+plot4.set_xlabel('Time', fontsize=15)
+plot4.set_ylabel('Fgf4', fontsize=15)
 plt.show()
